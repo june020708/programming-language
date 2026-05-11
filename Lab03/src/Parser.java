3c67abc13acc740f0f912f47c9957a861a943cf6
june020708 <june020708@gmail.com>
2026-05-11 14:16:08 +0900

    Add files

---
+++ Lab03/src/Parser.java
@@ -0,0 +1,387 @@
+// Parser.java
+// Parser for language S
+
+public class Parser {
+    Token token;          // current token 
+    Lexer lexer;
+    String funId = "";
+
+    public Parser(Lexer scan) { 
+        lexer = scan;		  
+        token = lexer.getToken(); // get the first token
+    }
+  
+    private String match(Token t) {
+        String value = token.value();
+        if (token == t)
+            token = lexer.getToken();
+        else
+            error(t);
+        return value;
+    }
+
+    private void error(Token tok) {
+        System.err.println("Syntax error: " + tok + " --> " + token);
+        token=lexer.getToken();
+    }
+  
+    private void error(String tok) {
+        System.err.println("Syntax error: " + tok + " --> " + token);
+        token=lexer.getToken();
+    }
+  
+    public Command command() {
+    // <command> ->  <decl> | <stmt>
+	    if (isType()) {
+	        Decl d = decl();
+	        return d;
+	    }
+	    if (token != Token.EOF) {
+	        Stmt s = stmt();
+            return s;
+	    }
+	    return null;
+    }
+
+    private Decl decl() { 
+    // <decl>  -> <type> id [=<expr>]; 
+        Type t = type();
+	    String id = match(Token.ID);
+	    Decl d = null;
+	    if (token == Token.ASSIGN) {
+	        match(Token.ASSIGN);
+            Expr e = expr();
+	        d = new Decl(id, t, e);
+	    } else 
+            d = new Decl(id, t);
+	    
+	    match(Token.SEMICOLON);
+	    return d;
+    }
+
+    private Decls decls () {
+    // <decls> -> {<decl>}
+        Decls ds = new Decls ();
+	    while (isType()) {
+	        Decl d = decl();
+	        ds.add(d);
+	    }
+        return ds;             
+    }
+
+    private Type type () {
+    // <type>  ->  int | bool | string 
+        Type t = null;
+        switch (token) {
+	    case INT:
+            t = Type.INT; break;
+        case BOOL:
+            t = Type.BOOL; break;
+        case STRING:
+            t = Type.STRING; break;
+        default:
+	        error("int | bool | string");
+	    }
+        match(token);
+        return t;       
+    }
+  
+    private Stmt stmt() {
+    // <stmt> -> <stmts> | <assignment> | <ifStmt> | <whileStmt> | ...
+        Stmt s = new Empty();
+        switch (token) {
+	    case SEMICOLON:
+            match(token.SEMICOLON); return s;
+        case LBRACE:			
+	        match(Token.LBRACE);		
+            s = stmts();
+            match(Token.RBRACE);	
+	        return s;
+        case IF: 	// if statement 
+            s = ifStmt(); return s;
+        case WHILE:      // while statement 
+            s = whileStmt(); return s;
+        case DO:
+            s = doStmt(); return s;
+        case FOR:
+            s = forStmt(); return s;
+        // TODO: [case DO, FOR]
+        // student exercise
+            
+        case ID:	// assignment
+            s = assignment(); return s;
+	    case LET:	// let statement 
+            s = letStmt(); return s;
+	    case READ:	// read statement 
+            s = readStmt(); return s;
+	    case PRINT:	// print statment 
+            s = printStmt(); return s;
+        default:  
+	        error("Illegal stmt"); return null; 
+	    }
+    }
+  
+    private Stmts stmts () {
+    // <stmts> -> {<stmt>}
+        Stmts ss = new Stmts();
+	    while((token != Token.RBRACE) && (token != Token.END))
+	        ss.stmts.add(stmt()); 
+        return ss;
+    }
+
+    private Let letStmt () {
+    // <letStmt> -> let <decls> in <block> end
+	    match(Token.LET);	
+        Decls ds = decls();
+	    match(Token.IN);
+        Stmts ss = stmts();
+        match(Token.END);	
+        match(Token.SEMICOLON);
+        return new Let(ds, ss);
+    }
+
+    private Read readStmt() {
+    // <readStmt> -> read id;
+    	match(Token.READ);
+        Identifier id = new Identifier(match(Token.ID));
+        match(Token.SEMICOLON);
+        return new Read(id);
+    }
+
+    private Print printStmt() {
+    // <printStmt> -> print <expr>;
+    	match(Token.PRINT);
+        Expr e = expr();
+        match(Token.SEMICOLON);
+        return new Print(e);
+    }
+
+    private Stmt assignment() {
+    // <assignment> -> id = <expr>;   
+        Identifier id = new Identifier(match(Token.ID));
+        match(Token.ASSIGN);
+        Expr e = expr();
+        match(Token.SEMICOLON);
+        return new Assignment(id, e);
+    }
+
+    private If ifStmt () {
+    // <ifStmt> -> if (<expr>) then <stmt> [else <stmt>]
+        match(Token.IF);
+	    match(Token.LPAREN);
+        Expr e = expr();
+	    match(Token.RPAREN);
+        match(Token.THEN);
+        Stmt s1 = stmt();
+        Stmt s2 = new Empty();
+        if (token == Token.ELSE){
+            match(Token.ELSE); 
+            s2 = stmt();
+        }
+        return new If(e, s1, s2);
+    }
+
+    private While whileStmt () {
+    // <whileStmt> -> while (<expr>) <stmt>
+    	match(Token.WHILE);
+        match(Token.LPAREN);
+        Expr e = expr();
+        match(Token.RPAREN);
+        Stmt s = stmt();
+        return new While(e, s);
+    }
+    
+    // TODO: [Implement dowhileStmt]
+    private Stmts doStmt() {
+    	// check syntax <dowhileStmt> -> do <stmt> while (<expr>);
+    	// ==> generate AST of [<stmt> <whileStmt>]
+    	// student exercise
+        Stmts ss = new Stmts();
+        match(Token.DO);
+        Stmt s = stmt();
+        ss.stmts.add(s);
+        match(Token.WHILE);
+        match(Token.LPAREN);
+        Expr e = expr();
+        match(Token.RPAREN);
+        match(Token.SEMICOLON);
+        ss.stmts.add(new While(e, s));
+    	return ss;
+    }
+
+    // TODO: [Implement forStmt]
+    private Let forStmt() {
+    	// check syntax <forStmt> -> for (<type> id = <expr>; <expr>; id = <expr>) <stmt>
+    	// ==> generate AST of [let <type> id = <expr> in while(<expr>) <stmt> end]
+    	// student exercise
+        match(Token.FOR);
+        match(Token.LPAREN);
+        Type t = type();
+	    String id1 = match(Token.ID);
+        match(Token.ASSIGN);
+        Expr e1 = expr();
+        match(Token.SEMICOLON);
+        Expr e2 = expr();
+        match(Token.SEMICOLON);
+        String id2 = match(Token.ID);
+        match(Token.ASSIGN);
+        Expr e3 = expr();
+        match(Token.RPAREN);
+        Stmt s = stmt();
+        Decls ds = new Decls(new Decl(id1, t, e1));
+        Stmts ss = new Stmts(s);
+        ss.stmts.add(new Assignment(new Identifier(id2), e3));
+    	return new Let(ds, new Stmts(new While(e2, ss)));
+    }
+
+    private Expr expr () {
+    // <expr> -> <bexp> {& <bexp> | '|'<bexp>} | !<expr> | true | false
+        switch (token) {
+	    case NOT:
+	        Operator op = new Operator(match(token));
+	        Expr e = expr();
+            return new Unary(op, e);
+        case TRUE:
+            match(Token.TRUE);
+            return new Value(true);
+        case FALSE:
+            match(Token.FALSE);
+            return new Value(false);
+        }
+        
+        Expr e = bexp();
+        
+        // parse logical operations
+        while (token == Token.AND || token == Token.OR) {
+            Operator op = new Operator(match(token));
+            Expr b = bexp();
+            e = new Binary(op, e, b);
+        }
+        
+        return e;
+    }
+
+    private Expr bexp() {
+        // <bexp> -> <aexp> [ (< | <= | > | >= | == | !=) <aexp> ]
+        Expr e = aexp();
+
+        switch(token) {
+        case LT: case LTEQ: case GT: case GTEQ: case EQUAL: case NOTEQ:
+            Operator op = new Operator(match(token));
+            Expr a = aexp();
+            e = new Binary(op, e, a);
+        }
+        
+        return e;
+    }
+  
+    private Expr aexp () {
+        // <aexp> -> <term> { + <term> | - <term> }
+        Expr e = term();
+        while (token == Token.PLUS || token == Token.MINUS) {
+            Operator op = new Operator(match(token));
+            Expr t = term();
+            e = new Binary(op, e, t);
+        }
+        return e;
+    }
+  
+    private Expr term () {
+        // <term> -> <factor> { * <factor> | / <factor>}
+        Expr t = factor();
+        while (token == Token.MULTIPLY || token == Token.DIVIDE) {
+            Operator op = new Operator(match(token));
+            Expr f = factor();
+            t = new Binary(op, t, f);
+        }
+        return t;
+    }
+  
+    private Expr factor() { 
+        // <factor> -> [-](id | <call> | literal | '('<aexp> ')')
+        Operator op = null;
+        if (token == Token.MINUS) 
+            op = new Operator(match(Token.MINUS));
+
+        Expr e = null;
+        switch(token) {
+        case ID:
+            Identifier v = new Identifier(match(Token.ID));
+            e = v;
+            break;
+        case NUMBER: case STRLITERAL: 
+            e = literal();
+            break; 
+        case LPAREN: 
+            match(Token.LPAREN); 
+            e = aexp();       
+            match(Token.RPAREN);
+            break; 
+        default: 
+            error("Identifier | Literal"); 
+        }
+
+        if (op != null)
+            return new Unary(op, e);
+        else return e;
+    }
+
+    private Value literal( ) {
+        String s = null;
+        switch (token) {
+        case NUMBER:
+            s = match(Token.NUMBER);
+            return new Value(Integer.parseInt(s));
+        case STRLITERAL:
+            s = match(Token.STRLITERAL);
+            return new Value(s);
+        }
+        throw new IllegalArgumentException( "no literal");
+    }
+ 
+    private boolean isType( ) {
+        switch(token) {
+        case INT: case BOOL: case STRING: 
+            return true;
+        default: 
+            return false;
+        }
+    }
+    
+    public static void main(String args[]) {
+	    Parser parser;
+        Command command = null;
+	    if (args.length == 0) {
+	        System.out.print(">> ");
+	        Lexer.interactive = true;
+	        parser  = new Parser(new Lexer());
+	        do {
+	            if (parser.token == Token.EOF) 
+		        parser.token = parser.lexer.getToken();
+
+                try {
+                    command = parser.command();
+		            if (command != null) command.display(0);    // display AST 
+                } catch (Exception e) {
+                    System.err.println(e);
+                }
+		        System.out.print("\n>> ");
+	        } while(true);
+	    }
+    	else {
+	        System.out.println("Begin parsing... " + args[0]);
+	        parser  = new Parser(new Lexer(args[0]));
+	        do {
+	            if (parser.token == Token.EOF) 
+                    break;
+
+                try {
+		             command = parser.command();
+		             if (command != null) command.display(0);      // display AST
+                } catch (Exception e) {
+                    System.err.println(e); 
+                }
+	        } while (command != null);
+	    }
+    } //main
+} // Parser
